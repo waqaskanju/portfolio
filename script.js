652e72f3d0af6abe3804a6f747dfdfd065e14537
waqaskanju <waqaskanju@gmail.com>
2022-05-10 19:46:27 +0500

    fix the display none Bug

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function openNav() {
   document.getElementById("mylist").style.display="block";
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-  document.getElementById("mylist").style.display="none";
-}
+
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    if( Math.max(window.innerWidth < 768)){
+      document.getElementById("mylist").style.display="none";
+    }
+  }
