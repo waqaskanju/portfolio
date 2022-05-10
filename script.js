a9151332dc02dc3015a2811b72beaa2eaa33ed38
waqaskanju <waqaskanju@gmail.com>
2022-05-10 20:35:06 +0500

    icon display changed

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
   document.getElementById("mylist").style.display="block";
+  document.getElementById("open_btn").innerText="";
+  document.getElementById("close_btn").innerHTML="&times;";
 }
-
-
 
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    document.getElementById("open_btn").innerHTML="&#9776;";
+    document.getElementById("close_btn").innerHTML="";
     if( Math.max(window.innerWidth < 768)){
       document.getElementById("mylist").style.display="none";
+      document.getElementById("close_btn").innerText="";
+    } else{
+     /*  document.getElementById("close_btn").innerHTML="&times;"; */
     }
   }
+
+function close_overlay(){
+  if( Math.max(window.innerWidth > 768)){
+    closeNav();
+  }
+}
+
+
+  
