e83da3eee9a6849a29875b274350221ecf49f214
waqaskanju <waqaskanju@gmail.com>
2022-05-10 19:26:46 +0500

    js file dead code removed

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/* function openNavigation() {
- const links = document.getElementById("mylinks");
- const bar = document.getElementById("bar")
-  if (links.style.display === "flex") {
-    links.style.display = "none";
-    bar.classList.remove("fa-times");
-    bar.classList.add("fa-bars");
-
-  } else {
-    links.style.display = "flex";
-    links.style.flexDirection="column"
-    bar.classList.remove("fa-bars");
-    bar.classList.add("fa-times");
-  }
-} */
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
